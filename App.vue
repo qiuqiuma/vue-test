85ec36046a6b2a26220e7948905427a2f70f10a8
qiuqiuma <1042161954@qq.com>
2023-09-17 17:19:22 +0800

    提交

---
+++ App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Test from './compoents/Test.vue'
+const arr = [1,2,3]
+</script>
+
+<template>
+  <div>
+          <!-- <div>1</div>
+      <div>2</div>
+      <div>3</div> -->
+    <Test>
+      <div v-for="(item,index) in arr" :key="index"> {{ item }}</div>
+    </Test>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ compoents/Test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+<script setup>
+import { useSlots } from "vue";
+const slots = useSlots();
+const res = slots.default();
+debugger
+</script>
+<style></style>
